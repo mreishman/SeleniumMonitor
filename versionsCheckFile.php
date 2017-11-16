34e0f9fae7708eec54712a037db2baf3ecc85720
Matt Reishman <mreishman@users.noreply.github.com>
2017-11-16 12:54:05 -0600

    Create versionsCheckFile.php

---
+++ versionsCheckFile.php
@@ -0,0 +1,12 @@
+<?php
+
+$versionCheckArray = array(
+	'version'		=> '1.1',
+	'versionList'		=> array(
+		'1.1'	        => array(
+			'branchName'	=> '1.1Update',
+			'releaseNotes'	=> '<ul><li>Test</li></ul>'
+		),
+	)
+);
+?>
