f70ef2ace65a9a7360b32307aa37457870ee7ae2
Matt Reishman <mreishman@users.noreply.github.com>
2017-12-10 09:44:52 -0600

    Update versionsCheckFile.php

--- versionsCheckFile.php
+++ versionsCheckFile.php
@@ -1,11 +1,15 @@
 <?php
 
 $versionCheckArray = array(
-	'version'		=> '1.0',
+	'version'		=> '1.1',
 	'versionList'		=> array(
 		'1.0'	        => array(
 			'branchName'	=> '1.0Update',
 			'releaseNotes'	=> '<ul><li>Initial Commit</li></ul>'
+		),
+		'1.1'	        => array(
+			'branchName'	=> '1.1Update',
+			'releaseNotes'	=> '<ul><li>Features<ul><li>Added ability to change website pointing to while running</li><li>Website input keeps data after starting to run a new test</li><li>Better disconnected interface</li><li>Added custom default base url</li><li>Option to re-run tests</li><li>Added FAQ Page</li><li>Added more test information to running test dropdown menu</li><li>Added check if node is open before starting to run a new test</li></ul></li><li>Bug Fixes<ul><li>Updated browser icons</li><li>Added custom timeout vars for data requests</li><li>Fixed issue with php unit verify poll not working</li></ul></li></ul>'
 		)
 	)
 );
