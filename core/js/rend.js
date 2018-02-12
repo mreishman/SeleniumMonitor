0ac3d001f830a1416615af92e735c3bc4bd5cfe6
Matt Reishman <matt.reishman@Me.com>
2018-02-12 13:54:28 -0600

    continues with compare #60

--- core/js/rend.js
+++ core/js/rend.js
@@ -3,10 +3,28 @@ function showRender(divId, renderInfo)
 	var item = $("#storage .container").html();
 	item = item.replace(/{{file}}/g, renderInfo["file"]);
 	item = item.replace(/{{website}}/g, renderInfo["website"]);
+	item = item.replace(/{{totalCount}}/g, getCountOfBlockType(renderInfo["info"], "block"));
+	item = item.replace(/{{failCount}}/g, getCountOfBlockType(renderInfo["info"], "blockFail"));
+	item = item.replace(/{{errorCount}}/g, getCountOfBlockType(renderInfo["info"], "blockError"));
 	$("#"+divId).append(item);
 }
 
 function generateProgressBlocks()
 {
 
+}
+
+function getCountOfBlockType(info, typefind)
+{
+	var keysInfo = Object.keys(info);
+	var keysInfoLength = keysInfo.length;
+	var count = 0;
+	for (var i = 0; i < keysInfoLength; i++)
+	{
+		if(info[keysInfo[i]]["result"].indexOf(typefind) > -1)
+		{
+			count++;
+		}
+	}
+	return count;
 }
